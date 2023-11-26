cc01732c4e23d0a3a94328059d04d19bb2048f19
Praise Dare <praisedare27@gmail.com>
2023-11-26 18:39:20 +0100

    Add hover state for task incomplete status icon

--- app.js
+++ app.js
@@ -236,7 +236,7 @@ const TaskItemProto = ({
         return {
             incomplete: {
                 src: `
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32" fill="none">
+                    <svg class="setup-task__status-icon--incomplete" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 32 32" fill="none">
                         <circle cx="16" cy="16" r="12" stroke="#8a8a8a" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="4 6" />
                     </svg>
                 `,
